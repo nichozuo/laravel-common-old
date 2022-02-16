f2a4702c937fdbd221dad50a2ddd21e374dad2a6
xiaowu <xiaowu@xxy.cn>
2022-02-16 12:18:38 +0800

    修复数据表没有备注的情况下创建不了test文件的bug

--- src/DevTools/Helpers/ReflectHelper.php
+++ src/DevTools/Helpers/ReflectHelper.php
@@ -37,6 +37,7 @@ class ReflectHelper
         $arr1 = [];
         foreach ($arr as $item) {
             $t1 = explode('\'', $item);
+            if (count($t1) < 3) continue;
             $t2 = explode('|', $t1[3]);
             $t3 = explode('#', $t1[4]);
             $t4 = [
